2b5c687b8c963b3c010878935bdb9b8c8f43eece
Etienne Kornobis <ekornobis@gmail.com>
2019-07-04 11:09:42 +0200

    Format init_repnots as scripts for usage with pip install -e

--- repnots/repnots.py
+++ repnots/repnots.py
@@ -2,14 +2,10 @@
 
 """Main module."""
 
-#! /usr/bin/env python
-
 import os
 import glob
-import argparse
-import subprocess
 import shutil
-import yaml
+
 from jinja2 import Template
 from nbconvert import TemplateExporter, HTMLExporter
 
@@ -74,88 +70,3 @@ def copy_conda_envs(conda_env_dir, outdir):
     os.makedirs(env_dir)
     for env_yaml_file in glob.glob(os.path.join(conda_env_dir, "*.yml")):
         shutil.copy(env_yaml_file, env_dir)
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Jupyter notebooks based workflow")
-
-    parser.add_argument(
-        "-o", "--outdir", required=True, help="Path to output directory"
-    )
-    parser.add_argument(
-        "-t",
-        "--notebook_templates_dir",
-        help="Path to directory with jupyter notebooks to use as templates",
-        default=os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "notebook_templates"
-        ),
-        type=directory_path_type,
-    )
-    parser.add_argument(
-        "-s",
-        "--snakefile_template",
-        help="Path to the snakefile jinja template file",
-        default=os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "templates", "basic_snakefile"
-        ),
-    )
-    parser.add_argument(
-        "-y",
-        "--yaml_config_template",
-        help="Path to the YAML config file template",
-        default=os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "templates", "config.yaml"
-        ),
-    )
-    parser.add_argument(
-        "-e",
-        "--conda_env_dir",
-        help="Path to the directory with all the conda environment yaml files",
-        type=directory_path_type,
-    )
-
-    return parser.parse_args()
-
-
-def directory_path_type(string):
-
-    # In case the argument is a proper directory, return the absolute path
-    if os.path.isdir(string):
-        return os.path.abspath(string)
-    else:
-        raise NotADirectoryError(string)
-
-
-def main():
-
-    args = parse_arguments()
-
-    # Create ouput directory
-    os.makedirs(args.outdir)
-
-    if args.conda_env_dir:
-        copy_conda_envs(args.conda_env_dir, args.outdir)
-
-    config_file = make_yaml_config(args.outdir, args.yaml_config_template)
-
-    with open(config_file, "r") as yml:
-        config = yaml.safe_load(yml)
-
-    make_snakefile(
-        args.notebook_templates_dir,
-        args.snakefile_template,
-        os.path.join(args.outdir, "Snakefile"),
-        config["EXECUTED_NOTEBOOKS_DIR"],
-    )
-
-    os.chdir(args.outdir)
-    subprocess.run(["snakemake", "--use-conda", "--use-singularity"])
-
-    for notebook in glob.glob(
-        os.path.join(config["EXECUTED_NOTEBOOKS_DIR"], "*.ipynb")
-    ):
-        export_notebook_to_html(notebook)
-
-
-if __name__ == "__main__":
-    main()
--- setup.py
+++ setup.py
@@ -5,46 +5,49 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest', ]
+test_requirements = ["pytest"]
 
 setup(
     author="Etienne Kornobis",
-    author_email='ekornobis@gmail.com',
+    author_email="ekornobis@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Reproductible data analysis framework with notebooks using Snakemake, Jupyter, Papermill, Conda and Singularity.",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='repnots',
-    name='repnots',
-    packages=find_packages(include=['repnots']),
+    keywords="repnots",
+    name="repnots",
+    packages=find_packages(include=["repnots"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/khourhin/repnots',
-    version='0.3.0',
+    url="https://github.com/khourhin/repnots",
+    version="0.3.0",
     zip_safe=False,
+    entry_points={
+        "console_scripts": ["init_repnots=repnots.scripts.init_repnots:main"]
+    },
 )
