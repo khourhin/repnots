e098e849e3854f50f7ce344bd971929f35516a55
Etienne Kornobis <ekornobis@gmail.com>
2019-07-05 00:21:21 +0200

    Adding cell results test with scrapbook

--- tests/test_repnots.py
+++ tests/test_repnots.py
@@ -5,15 +5,31 @@
 
 import pytest
 import papermill as pm
+import scrapbook as sb
 import subprocess
 
 from repnots import repnots
 
-
-# @pytest.fixture(scope="module")
-# def setup(tmpdir):2
+# From https://github.com/diana-hep/pyhf/blob/master/tests/test_notebooks.py
+@pytest.fixture()
+def common_kwargs(tmpdir):
+    outputnb = tmpdir.join("output.ipynb")
+    return {"output_path": str(outputnb)}
 
 
 def test_init_repnots(tmpdir):
-
+    """ Launch complete init_repnots script
+    """
     subprocess.run(["init_repnots", "-o", tmpdir])
+
+
+def test_notebook(common_kwargs):
+    """ Test a specific notebook using scrapbook
+    """
+    pm.execute_notebook(
+        "repnots/scripts/notebook_templates/workflow.ipynb", **common_kwargs
+    )
+
+    nb = sb.read_notebook(common_kwargs["output_path"])
+
+    assert nb.scraps["results"].data == 2
